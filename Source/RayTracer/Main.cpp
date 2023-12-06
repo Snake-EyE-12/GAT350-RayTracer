b23747b08d4dfbed87b6c6352c29a25d8533d5bd
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2023-12-05 19:26:09 -0700

    Scene

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -7,8 +7,11 @@
 #include "Material.h"
 #include "Sphere.h"
 #include <vector>
+#include "Plane.h"
+#include "Triangle.h"
+#include <glm/gtx/color_space.hpp>
 
-int main(int argc, char**) { 
+int main(int argc, char**) {
 	seedRandom((unsigned int)time(nullptr));
 
 	std::cout << "Hello World!" << std::endl;
@@ -19,28 +22,64 @@ int main(int argc, char**) {
 	renderer.CreateWindow("RayTracing", 800, 600);
 	Canvas canvas(800, 600, renderer);
 
-
 	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
-	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 1 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 70.0f, aspectRatio);
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
 
-	Scene scene(10); // sky color could be set with the top and bottom color
+
+	Scene scene(8, glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f });
 	scene.SetCamera(camera);
 
-	
-	// create material
-	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0, 0, 1 });
-	auto metal = std::make_shared<Metal>(color3_t{ 1, 1, 1 }, 0.0f);
 
-	// create objects -> add to scene
-	for (int i = 0; i < 10; i++)
+	for (int x = -10; x < 10; x++)
 	{
-		std::shared_ptr<Material> material = ((int)random(0, 2) == 0) ? std::dynamic_pointer_cast<Material>(lambertian) : std::dynamic_pointer_cast<Material>(metal);
-		auto sphere = std::make_unique<Sphere>(random({ -2, -2, -1 }, { 2, 2, -7 }), random(0.2, 0.4), material);
-		scene.AddObject(std::move(sphere));
-	
-	
+		for (int z = -10; z < 10; z++)
+		{
+
+			std::shared_ptr<Material> material;
+
+			// create random material
+			float r = random01();
+			if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
+			else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
+			else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
+			else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
+
+			// set random sphere radius
+			float radius = random(0.2f, 0.3f);
+			// create sphere using random radius and material
+			auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f) }, radius, material);
+			// add sphere to the scene
+			scene.AddObject(std::move(sphere));
+		}
+	}
+
+	std::shared_ptr<Material> material = std::make_shared<Lambertian>(color3_t{ 0.2 });
+	auto plane = std::make_unique<Plane>(glm::vec3{ 0, 0,  0 }, glm::vec3{ 0, 1, 0 }, material);
+	scene.AddObject(std::move(plane));
+	/*
+	std::shared_ptr<Material> triangleMaterial = std::make_shared<Lambertian>(color3_t{ 0.9f, 0.5f, 0.5f });
+	auto tri = std::make_unique<Triangle>(glm::vec3{ 0, 1,  -3 }, glm::vec3{ 2, 1, -3 }, glm::vec3{ 1.5f, 2.5f, -3 }, triangleMaterial);
+	scene.AddObject(std::move(tri));
+	{ // Planes
+		std::shared_ptr<Material> material = std::make_shared<Lambertian>(color3_t{ 0.5f, 1.0f, 0.5f });
+		auto planeGreen = std::make_unique<Plane>(glm::vec3{ 20, 0, 0 }, glm::vec3{ -1, 0, 0 }, material);
+		scene.AddObject(std::move(planeGreen));
+
+		material = std::make_shared<Lambertian>(color3_t{ 0.9f, 0.4f, 0.4f });
+		auto planeRed = std::make_unique<Plane>(glm::vec3{ -20, 0, 0 }, glm::vec3{ 1, 0, 0 }, material);
+		scene.AddObject(std::move(planeRed));
+
+		// White Walls
+		material = std::make_shared<Lambertian>(color3_t{ 0.95f, 0.95f, 0.85f });
+		auto planeWhite = std::make_unique<Plane>(glm::vec3{ 0, -20, 0 }, glm::vec3{ 0, 1, 0 }, material);
+		scene.AddObject(std::move(planeWhite));
+
+		planeWhite = std::make_unique<Plane>(glm::vec3{ 0, 0, -40 }, glm::vec3{ 0, 0, 1 }, material);
+		scene.AddObject(std::move(planeWhite));
+	}*/
+
 	canvas.Clear({ 0, 0, 0, 1 });
-	scene.Render(canvas, 30);
+	scene.Render(canvas, 10);
 	canvas.Update();
 
 	bool quit = false;
