523dba90331e336c57169ded86abe94ed75afa21
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2023-12-06 12:12:43 -0700

    Scene Up to date

--- Source/RayTracer/Scene.cpp
+++ Source/RayTracer/Scene.cpp
@@ -2,8 +2,10 @@
 #include "Canvas.h"
 #include "MathUtils.h"
 #include "Random.h"
+#include <iostream>
+#include <iomanip>
 
-void Scene::Render(Canvas& canvas, int numSamples)
+void Scene::Render(Canvas& canvas, int numSamples, int depth)
 {
 	// cast ray for each point (pixel) on the canvas
 	for (int y = 0; y < canvas.GetSize().y; y++)
@@ -30,8 +32,10 @@ void Scene::Render(Canvas& canvas, int numSamples)
 
 				// cast ray into scene
 				// add color value from trace
+				// cast ray into scene
+				// set color value from trace
 				raycastHit_t raycastHit;
-				color += Trace(ray, 0, 100, raycastHit, m_depth);
+				color += Trace(ray, 0, 100, raycastHit, depth);
 			}
 
 			// draw color to canvas point (pixel)
@@ -39,6 +43,7 @@ void Scene::Render(Canvas& canvas, int numSamples)
 			color /= numSamples;
 			canvas.DrawPoint(pixel, color4_t(color, 1));
 		}
+		std::cout << std::setprecision(2) << std::setw(5) << ((y / canvas.GetSize().y) * 100) << "%\n";
 	}
 }
 color3_t Scene::Trace(const ray_t& ray, float minDistance, float maxDistance, raycastHit_t& raycastHit, int depth)
--- Source/RayTracer/Scene.h
+++ Source/RayTracer/Scene.h
@@ -10,14 +10,13 @@
 class Scene
 {
 public:
-	Scene(int depth = 5) : m_depth{ depth } {}
-	Scene(int depth, const color3_t& topColor, const color3_t& bottomColor) :
-		m_depth{ depth },
+	Scene() = default;
+	Scene(const color3_t& topColor, const color3_t& bottomColor) :
 		m_topColor{ topColor },
 		m_bottomColor{ bottomColor }
 	{}
 
-	void Render(class Canvas& canvas, int numSamples);
+	void Render(class Canvas& canvas, int numSamples, int depth);
 	color3_t Trace(const ray_t& ray, float minDistance, float maxDistance, raycastHit_t& raycastHit, int depth);
 	void AddObject(std::unique_ptr<Object> object) { m_objects.push_back(std::move(object)); }
 
@@ -26,7 +25,6 @@ public:
 private:
 	std::shared_ptr<Camera> m_camera;
 
-	int m_depth{ 5 };
 	color3_t m_topColor{ 1 };
 	color3_t m_bottomColor{ 0.5f };
 	std::vector<std::unique_ptr<Object>> m_objects;
